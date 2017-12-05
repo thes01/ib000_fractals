f07e23f176772e25953d77274f021be33027befe
Miroslav Šerý <xsery@fi.muni.cz>
2017-12-05 18:39:56 +0100

    refactor code

--- dragon_curve.py
+++ dragon_curve.py
@@ -4,7 +4,7 @@ import turtle
 
 
 def turtle_shift(my_turtle, pos_delta, angle_delta):
-    """ A helper method that shifts the turtle without drawing a line """
+    """ A helper method that shifts the turtle to the end of dragon curve without drawing a line """
     my_turtle.penup()
     new_x = my_turtle.pos()[0] - pos_delta[1]
     new_y = my_turtle.pos()[1] + pos_delta[0]
@@ -22,22 +22,22 @@ def dc_from_beginning(my_turtle, level, dist=10):
     if level > 1:
         dc_from_beginning(my_turtle, level - 1, dist)
     else:
-        my_turtle.backward(dist)
+        my_turtle.forward(dist)
 
     pos_delta = (my_turtle.pos()[0] - start_pos[0], my_turtle.pos()[1] - start_pos[1])
     angle_delta = my_turtle.heading() - start_angle
 
-    dc_from_middle(my_turtle, pos_delta, angle_delta, level, dist)
+    dc_from_end(my_turtle, pos_delta, angle_delta, level - 1, dist)
 
 
-def dc_from_middle(my_turtle, pos_delta, angle_delta, level, dist=10):
-    """ Starts drawing the curve from middle - Shift to the end and then draw from begin """
+def dc_from_end(my_turtle, pos_delta, angle_delta, level, dist=10):
+    """ Starts drawing the curve from end - Shifts to the end and then draws as from begin """
     turtle_shift(my_turtle, pos_delta, angle_delta)
 
-    if level > 1:
-        dc_from_beginning(my_turtle, level - 1, dist)
+    if level > 0:
+        dc_from_beginning(my_turtle, level, dist)
     else:
-        my_turtle.backward(dist)
+        my_turtle.forward(dist)
 
     my_turtle.left(90)
     turtle_shift(my_turtle, pos_delta, angle_delta)
@@ -46,7 +46,7 @@ bob = turtle.Turtle()
 bob.speed(0)
 bob.left(180)
 bob.hideturtle()
-# bob._tracer(0,0)
+# bob._tracer(0, 0)
 
-dc_from_beginning(bob, 10, 10)
+dc_from_beginning(bob, 13, 5)
 turtle.done()
